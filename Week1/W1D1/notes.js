0963f71226294e0c7d50a8befc1a7646a4c77088
baylend123 <baylendoss12@gmail.com>
2022-01-17 13:23:27 -0600

    First commit

---
+++ Week1/W1D1/notes.js
@@ -0,0 +1,37 @@
+// <=  that is a comment
+
+// console.log() prints to the terminal 
+
+
+// Examples of strings
+
+
+// Compute arithmetic using +, -, *, /, %,
+
+
+// Examples of NaN
+
+
+// EXamples of boolean operators &&, ||, !
+
+
+// Examples of comparison operators >, >=, ===, <, <=,
+
+
+//  De Morgans law
+
+
+// Examples of Pemdas
+
+
+// example of parenthesis 
+
+
+// Difference between == and === 
+
+
+//  Examples of ++, --, +=, -=, /=, 
+
+
+// let a few variables string, int, and bool,  print their values to the console 
+
