7ee8936ce5861a5f1f06ffc1a20c746835d20963
baylend123 <baylendoss12@gmail.com>
2022-01-19 14:50:06 -0600

    Day 2

---
+++ Week1/W1D2/notes.js
@@ -0,0 +1,22 @@
+// String concat examples 
+
+
+// String indexing examples -- ZERO INDEXING FOREVER
+
+
+// length and indexof examples
+
+
+// Console lof plus param function
+
+
+// return vs console log functions 
+
+
+// Arithmetic function 
+
+
+// Console log and return in a function 
+
+
+// Multiple arithmetic operations, ie average 
