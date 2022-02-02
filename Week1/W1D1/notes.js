9defa00f55a97da8706640976d0b74d48416f70c
baylend123 <baylendoss12@gmail.com>
2022-02-02 15:21:39 -0600

    Week 3 day 2 project

--- Week1/W1D1/notes.js
+++ Week1/W1D1/notes.js
@@ -1,37 +1,81 @@
 // <=  that is a comment
 
+// Hello this is my comment 
+
+// Hello this a comment 
+
 // console.log() prints to the terminal 
+// console.log('Hey My name is baylen')
+// console.log(true)
+// console.log(1 + 3)
+// console.log('Baylen' + ' Doss')
 
+// // Examples of strings '' vs  ""
+// 'Hello this is a string'
 
-// Examples of strings
-
-
-// Compute arithmetic using +, -, *, /, %,
-
+// // Compute arithmetic using +, -, *, /, %,
+// console.log(1 + 2)
+// console.log(2-4)
+// console.log(2 * 10)
+// console.log(100 / 4)
+// console.log(100 % 2)
 
 // Examples of NaN
+// console.log(undefined + 1)
+// console.log(isNaN('Hello'))
+// console.log(isNaN(NaN))
 
-
-// EXamples of boolean operators &&, ||, !
-
+// // Examples of boolean operators &&, ||, !
+// console.log(true && false) // = False
+// console.log(true && true) // true
+// console.log(true || false) // true
+// console.log(!true || !false) // true
+// console.log(!true && !false) // false
+// console.log(!(true || false) === (!true && !false)) // true
 
 // Examples of comparison operators >, >=, ===, <, <=,
-
-
-//  De Morgans law
-
-
-// Examples of Pemdas
-
-
-// example of parenthesis 
+// console.log(10 < 100) // true
+// console.log(100 > 10) // true
+// console.log(100 >= 10) // true
+// console.log(100 <= 10) // false
+// console.log(100 === 10) // false
+// console.log(100 === 100) // true
 
 
 // Difference between == and === 
+// console.log('5' === 5)
+
+
+// Pemdas 
+// Parens exponent multiply divide add subtract
+// console.log(2 + 3 * 4 - 1 / 2**3)
 
 
 //  Examples of ++, --, +=, -=, /=, 
+// let myNumber = 4
+// let mySuperCoolVariable = 1
+// myNumber+= 4
+// myNumber = myNumber + 4
+// console.log(myNumber)
+// myNumber -= 4
+// console.log(myNumber)
+// myNumber /= 2
+// console.log(myNumber)
 
 
-// let a few variables string, int, and bool,  print their values to the console 
+// console.log(myNumber)
+// myNumber++
+// console.log(myNumber)
+// myNumber++
+// console.log(myNumber)
+// myNumber--
+// console.log(myNumber)
+// myNumber--
+// console.log(myNumber)
+
+
+
+
+
+
 
